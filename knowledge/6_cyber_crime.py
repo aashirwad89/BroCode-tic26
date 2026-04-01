8639ff0b747df97cdb3b68b9c158b2f310624829
Aman Kumar <167611489+amannsinghh3028@users.noreply.github.com>
2026-04-01 13:31:08 +0530

    Add cyber crime laws and actions reference

---
+++ knowledge/6_cyber_crime.py
@@ -0,0 +1,16 @@
+laws = {
+    "types": [
+        "Online harassment",
+        "Fake profiles",
+        "Blackmail",
+        "Private photo leak"
+    ],
+    
+    "action": [
+        "Report to cyber cell",
+        "Block offender",
+        "Save evidence"
+    ],
+    
+    "helpline": "1930"
+}
