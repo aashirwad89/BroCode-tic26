cdfddf615f75fac8cf3e5bd101a74d11cbfe1014
Aman Kumar <167611489+amannsinghh3028@users.noreply.github.com>
2026-04-01 13:31:38 +0530

    Add police legal procedure rights information

---
+++ knowledge/7_police_legal_procedure.py
@@ -0,0 +1,8 @@
+laws = {
+    "rights": [
+        "FIR can be filed at any police station",
+        "No arrest at night (in most cases)",
+        "Female officer required",
+        "Free legal aid"
+    ]
+}
