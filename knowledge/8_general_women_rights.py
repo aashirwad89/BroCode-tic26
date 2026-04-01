977e1f92208dfdbc14eef50e4b74d0abde8a5302
Aman Kumar <167611489+amannsinghh3028@users.noreply.github.com>
2026-04-01 13:32:05 +0530

    Add general women rights information in Python file

---
+++ knowledge/8_general_women_rights.py
@@ -0,0 +1,14 @@
+laws = {
+    "fundamental_rights": [
+        "Right to equality",
+        "Right to dignity",
+        "Right to education",
+        "Right to work"
+    ],
+    
+    "important_points": [
+        "Women have special protection laws",
+        "Law supports safety and justice",
+        "Proof and fairness are important"
+    ]
+}
