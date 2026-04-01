a1aabe722b04fe59b51fb0aacce8b33a3da0f8e8
Aman Kumar <167611489+amannsinghh3028@users.noreply.github.com>
2026-04-01 13:30:34 +0530

    Add property inheritance laws in 5_property_inheritance.py

---
+++ knowledge/5_property_inheritance.py
@@ -0,0 +1,9 @@
+laws = {
+    "act": "Hindu Succession Act, 2005",
+    
+    "rights": [
+        "Equal right in father's property",
+        "Daughter = Son (same rights)",
+        "Right to inherit property"
+    ]
+}
