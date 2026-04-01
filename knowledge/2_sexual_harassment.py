5f2e7c656e2698d0bdb6c6d152c12a34910e886c
Aman Kumar <167611489+amannsinghh3028@users.noreply.github.com>
2026-04-01 13:29:00 +0530

    Add POSH Act, 2013 details to sexual harassment file

---
+++ knowledge/2_sexual_harassment.py
@@ -0,0 +1,16 @@
+laws = {
+    "act_name": "POSH Act, 2013",
+    
+    "definition": "Unwanted physical contact, comments, messages",
+    
+    "examples": [
+        "Bad touch",
+        "Dirty comments",
+        "Sending inappropriate messages"
+    ],
+    
+    "workplace_rights": [
+        "Internal Complaints Committee (ICC)",
+        "File complaint within 3 months"
+    ]
+}
